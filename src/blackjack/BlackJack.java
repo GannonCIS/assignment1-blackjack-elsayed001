f765b5c4fc98f20b919703395aee7107c28b6356
Youssef Elsayes <elsayed001@gannon.edu>
2020-02-03 15:58:35 -0500

    implemented init deck and added printdeck for testing

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -16,6 +16,9 @@ public class BlackJack {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Deck myDeck = new Deck();
+        myDeck.printDeck();
+        
     }
     
 }
--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -10,27 +10,43 @@ package blackjack;
  * @author gubotdev
  */
 public class Deck {
-    private Card[] myCards = new Card[52];
-  private  String[] ranks = {};
-    private String[] suits = {};
-    private int nextCard = 0;
-    
-    public Deck(){
-        
-    }
-    private void initDeck(){
-          
-}
 
-private void shuffle(){
-    
-}
+    private Card[] myCards = new Card[52];
+    private String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9","10"
+            , "Jack" + "", "Queen", "King"};
+    private String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+    private int nextCard = 0;
+
+    public Deck() {
+        initDeck();
+
+    }
+
+    private void initDeck() {
+     int index = 0;
+        for(int s = 0; s < suits.length; s++){
+            for(int r = 0; r < ranks.length; r++){
+                myCards[index] = new Card(ranks[r], suits[s]);
+                index++;
+                
+                
+            }
+        }
+    }
+
+    private void shuffle() {
+
+    }
 // printDeck is for testing only -- delete me
 
-public void printDeck(){
-    
-}
-public Card dealCard(){
-    
-    return null; // do not forget to change this!!!!!!!!!!!!!!!!!!!
+    public void printDeck() {
+        for (int i = 0; i < myCards.length; i++) {
+            System.out.println(myCards[i].RANK + "of" + myCards[i].SUIT);
+        }
+    }
+
+    public Card dealCard() {
+
+        return null; // do not forget to change this!!!!!!!!!!!!!!!!!!!
+    }
 }
